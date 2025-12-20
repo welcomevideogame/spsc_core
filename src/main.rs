1d26cb79176a567070cd7d1240281ee3600796fe
welcomevideogame <99519510+welcomevideogame@users.noreply.github.com>
2025-12-20 12:57:39 -0500

    feat: Add error type to sender

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use std::{error::Error, time::Duration};
+
+use tokio::join;
+
+use crate::spsc::{Receiver, Sender};
+
 mod spsc;
+
+async fn send_loop(tx: Sender<i32>) -> Result<(), Box<dyn Error>> {
+    loop {
+        tx.send(10)?;
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+async fn recv_loop(rx: Receiver<i32>) -> Result<(), ()> {
+    loop {
+        rx.recv();
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let (tx, rx) = spsc::channel::<i32>(10);
+    let (x, y) = join!(send_loop(tx), recv_loop(rx));
+    dbg!(x, y);
 }
--- src/spsc/error.rs
+++ src/spsc/error.rs
@@ -1,5 +1,15 @@
-use std::marker::PhantomData;
+use std::{
+    error::Error,
+    fmt::{Debug, Display},
+};
 
-pub struct SendError<T> {
-    _a: PhantomData<T>,
+#[derive(Debug, Clone)]
+pub struct SendError<T>(pub T);
+
+impl<T> Display for SendError<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "send failed, channel is closed")
+    }
 }
+
+impl<T: Debug> Error for SendError<T> {}
